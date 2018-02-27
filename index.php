1e9eee3d35621d53fd253264c1e5f78fbdd48ea8
weichuan.tran <weichuan.tran@gmail.com>
2018-02-27 19:14:22 +0100

    update 1

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PorteFolio_Weichuan</title>
+    <link rel="stylesheet" href="sass-portefolio/stylesheets/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  </head>
+  <body>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+      <header>
+        <nav>
+          <div class="NavEnTete">
+            <ul>
+              <li>Who I Am ?</li>
+              <li> Skills  </li>
+              <li>My Projets</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <div class="main">
+        <div class="block">
+          <div class="block_Left">
+            <div class="block_Left_Up">
+              <div class="Rectangle">
+                <h1 class="Title">Contact</h1>
+                <div class="Contact_Icons">
+                  <i class="far fa-envelope fa-2x"></i><br/>
+                  <i class="fas fa-phone fa-2x"></i><br/>
+                  <i class="fas fa-home fa-2x"></i><br/>
+                  <i class="fab fa-github fa-2x"></i><br/>
+                </div>
+                <div class="Contact_Details">
+                  <p> weichuan.tran@gmail.com</p>
+                  <p> 0472/32 65 51</p>
+                  <p>Boulevar du jardin Botanique 6, bt 003, 1000 Bruxelles</p>
+                  <p>https://github.com/weichuan888</p>
+                </div>
+              </div>
+            </div>
+            <div class="block_Left_Down">
+              <div class="Rectangle">
+                <h1 class="Title">Hobbies</h1>
+                <div class="Contenu">
+                  <p><span>Music:</span> Classic and Pop</p>
+                  <p><span>Books:</span> Fantasy </p>
+                  <p><span>Movies:</span> Comedy and Action</p>
+                  <p><span>Sports:</span> Badminton and Tennis</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div class="block_center">
+            <div class="Who">
+              <h1>WEICHUAN TRAN</h1>
+              <img src="./img/testLogo.jpg" alt="photo">
+              <p>I am a Junior Web Developer, who wants to develop in Front-End.<br/>
+                I like challenges and learn new technologies to deepen my skills.<br/>
+                Motivation, rigor and listening are the watchwords of my professional behavior.
+              </p>
+            </div>
+          </div>
+          <div class="block_Right">
+            <div class="block_Right_Up">
+              <div class="Rectangle">
+                <h1 class="Title">Work Experience</h1>
+                <div class="Contenu">
+                  <p><span>July 2017: </span> BSG du SHAPE - internship in accounting. </p>
+                  <p><span>October 2013 - January 2017:</span> Independant and manager of a bookstore.</p>
+                </div>
+
+              </div>
+            </div>
+            <div class="block_Right_Down">
+              <div class="Rectangle">
+                <h1 class="Title">Formations</h1>
+                <div class="Contenu">
+                  <p><span>2005-2011:</span> Institut of "Sainte-Famille" from Mons - Ursulines - CESS.</p>
+                  <p><span>2011-2013:</span> Umons - 1st Bachelor of Medecine.</p>
+                  <p><span>2013-2017:</span> IRAMPS Mons - Bachelor of Accounting option Management -> With Satisfaction.</p>
+                  <p><span>2017-2018 (Sep-Apr):</span> BECODE - BXL -> Formation: Junior Web Developer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+  </body>
+</html>
